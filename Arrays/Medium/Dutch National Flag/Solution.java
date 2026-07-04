484d867e9dad47aa6a46bf38ee11c55afc1b453b
Aman singh <amansingh3252@gmail.com>
2026-07-04 14:58:07 +0530

    Create Solution.java

---
+++ Arrays/Medium/Dutch National Flag/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void sortColors(int[] arr) {
+        //using dutch national flag algorithm
+        int low=0; int mid=0; int high=arr.length-1;
+
+        while(mid<=high){
+            if(arr[mid]==0){
+                swap(arr,low,mid);
+                low++; mid++;
+            }else if(arr[mid]==1){
+                mid++;
+            }else{
+                swap(arr,mid,high);
+                high--;
+            }
+        }
+        return;
+    }
+    static void swap(int [] arr, int first, int second){
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
+}
